72a2a2b45c9cbad9b142538842944152b6965e8c
Jason <jakiracofe10@gmail.com>
2016-11-21 23:45:22 -0500

    initial

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int findRepeating(int* arr, int n) {
+    map<int,int> count;
+    for (int i=0; i<n; i++) {
+        count[arr[i]]++;
+    }
+    for(map<int,int>::iterator it = count.begin();it != count.end();it++) {
+        if (it->second > 1) {
+            return it->first;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n = 10;
+    int arr[n] = {1,2,3,4,5,6,11,6,9,10};
+    int result = findRepeating(arr, n);
+    cout << result;
+    return 0;
+}
